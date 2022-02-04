a6ab618dc9a3fe0eec7dd4854c40911b1b3cef51
Firman Ismail <akayasha99@gmail.com>
2022-02-04 17:34:49 +0700

    tugas 2 js

---
+++ tugas2.js
@@ -0,0 +1,11 @@
+var number = 6;
+var string = "coconut bizare";
+var bool = false ;
+var desk
+var arr =["haha",3,false,"coco"]
+
+console.log(number);
+console.log(string);
+console.log(bool);
+console.log(desk);
+console.log(arr);
